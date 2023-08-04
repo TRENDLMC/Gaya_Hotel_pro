7a9436022ebaf0e927aeeaa1cb7d69720a939e23
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-08-04 16:43:35 +0900

    총 매출 확인

--- back/src/main/java/com/GaYaHole/Pro/controller/AdminController.java
+++ back/src/main/java/com/GaYaHole/Pro/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.GaYaHole.Pro.controller;
 
 
+import com.GaYaHole.Pro.repository.AccountRepository;
 import com.GaYaHole.Pro.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -18,15 +19,27 @@ public class AdminController {
     @Autowired
     RoomRepository roomRepository;
 
+    @Autowired
+    AccountRepository accountRepository;
+
     @Transactional
-    @PutMapping("/admin/modroom")
+    @PutMapping("/admin/modroom") //방 가격 수정
     public int modRoom(@RequestParam int roomnum, @RequestParam String roomprice){
 
         roomRepository.roomUpdate(roomnum, roomprice);
 
         System.out.println("확인");
 
-
         return 1;
+    }
+
+    @PostMapping("/admin/gProfit")
+    public int profit(){
+        int result=0;
+        result = accountRepository.totalAccounting();
+
+        System.out.println("============== 총 매출 확인용 : "+result+" ==============");
+
+        return result;
     }
 }
