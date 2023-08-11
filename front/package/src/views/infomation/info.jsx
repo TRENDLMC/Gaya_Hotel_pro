b6c0697f19b6da43a452802cd594b7ccf7697943
KangCheolgu <opopqwqw123@gmail.com>
2023-08-11 13:40:49 +0900

    코드 정리 및 룸 리스트 css 완료

--- front/package/src/views/infomation/info.jsx
+++ front/package/src/views/infomation/info.jsx
@@ -12,52 +12,51 @@ import Kakao from "./sections/Kakao.jsx";
 //scss
 
 const Information = () => {
-    const Location = useLocation();
-    const [open, setopen] = useState({
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false
-    });
+  const Location = useLocation();
+  const [open, setopen] = useState({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+  });
 
-    useEffect(() => {
-        const i_num = Location.state.num;
-        viewchage(i_num);
-    }, [Location])
+  useEffect(() => {
+    const i_num = Location.state.num;
+    viewchage(i_num);
+  }, [Location]);
 
-    const viewchage = (i_num) => {
-        setopen({ [i_num]: true });
-    }
+  const viewchage = (i_num) => {
+    setopen({ [i_num]: true });
+  };
 
-    return (
-        <div id="main-wrapper">
-            <Header />
-            <Container>
-                <Row>
-                    <Col md="3" style={{ border: "solid" }}>
-                        <Leftnavi />
-                    </Col>
-                    <Col md="9" style={{ border: "solid" }}>
-                        {open[1] && "1 칸"}
-                        {open[2] && "2 칸"}
-                        {open[3] && "3 칸"}
-                        {open[4] && <Kakao />}
-                        {open[5] && "5 칸"}
-                    </Col>
-                </Row>
-            </Container>
-            <div className="page-wrapper">
-                <div className="container-fluid">
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
-}
+  return (
+    <div id="main-wrapper">
+      <Header />
+      <Container>
+        <Row>
+          <Col md="3" style={{ border: "solid" }}>
+            <Leftnavi />
+          </Col>
+          <Col md="9" style={{ border: "solid" }}>
+            {open[1] && "1 칸"}
+            {open[2] && "2 칸"}
+            {open[3] && "3 칸"}
+            {open[4] && <Kakao />}
+            {open[5] && "5 칸"}
+          </Col>
+        </Row>
+      </Container>
+      <div className="page-wrapper">
+        <div className="container-fluid"></div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 Information.propTypes = {
-    classes: PropTypes.object
+  classes: PropTypes.object,
 };
 
 export default Information;
