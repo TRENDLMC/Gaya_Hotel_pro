280e35feb1b462ab2dba582ca2d56c01b7a631d8
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-08-11 12:20:34 +0900

    사용 안 하는 코드 삭제

--- back/src/main/java/com/GaYaHole/Pro/controller/ReviewController.java
+++ back/src/main/java/com/GaYaHole/Pro/controller/ReviewController.java
@@ -1,21 +1,12 @@
 package com.GaYaHole.Pro.controller;
 
-import com.GaYaHole.Pro.entity.Reservation;
+
 import com.GaYaHole.Pro.entity.Review;
-import com.GaYaHole.Pro.entity.Room;
-import com.GaYaHole.Pro.entity.User;
-import com.GaYaHole.Pro.repository.ReviewRepository;
 import com.GaYaHole.Pro.service.ReviewService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -25,8 +16,6 @@ public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
-
-
 
     @PostMapping("/write")
     public String regreview (@RequestBody Review review) {
